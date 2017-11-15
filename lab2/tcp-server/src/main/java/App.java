fa102f3f43ecb7ff5cc1276f07adbff468924006
dianaartiom <diana.artiom24@gmail.com>
2017-11-15 19:28:11 +0200

    Returned to a running instance of program.

--- lab2/tcp-server/src/main/java/App.java
+++ lab2/tcp-server/src/main/java/App.java
@@ -23,28 +23,12 @@ public class App {
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(socket.getInputStream()));
 
-        sendMessage2(out, new File(args[1]));
+        sendMessage(out, new File(args[1]));
         readResponse(in);
 
         out.close();
         in.close();
     }
-
-//    public void slowLoris(String[] args) throws IOException {
-//        Socket socket = new Socket(args[0], 80);
-//        socket.setSoTimeout(7000);
-//
-//        BufferedWriter out = new BufferedWriter(
-//                new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
-//        BufferedReader in = new BufferedReader(
-//                new InputStreamReader(socket.getInputStream()));
-//
-//        sendMessage(out, new File(args[1]));
-//        readResponse(in);
-//
-//        out.close();
-//        in.close();
-//    }
 
     private static void sendMessage2(BufferedWriter out, File request) throws IOException, InterruptedException {
         System.out.println(" * Request");
@@ -86,7 +70,7 @@ public class App {
         String line;
         while ((line = in.readLine()) != null) {
             System.out.println(line);
-            /* trebuie de pus o conditie ca sa se opreasca dupa ce o venit 0 */
+            if (line.equals("0")) { break; }
         }
     }
 
--- lab2/tcp-server/src/test/java/AppTest.java
+++ lab2/tcp-server/src/test/java/AppTest.java
@@ -9,24 +9,24 @@ import java.io.IOException;
 
 public class AppTest {
 
-    App app = new App();
-
-    @Rule
-    public ContiPerfRule i = new ContiPerfRule();
-    @Test
-    @PerfTest(invocations = 10000, threads = 20)
-    @Required(max = 1200, average = 250)
-    public void runMethodTest() throws IOException, InterruptedException {
-        String[] args = ("165.227.137.80 /home/diana/Desktop/lab.txt").split(" ");
-        app.run(args);
-    }
-
-    @Test
-    @PerfTest(invocations = 1, threads = 1)
-    @Required(max = 1200, average = 250)
-    public void serverTestRun() throws IOException, InterruptedException {
-
-    }
+//    App app = new App();
+//
+//    @Rule
+//    public ContiPerfRule i = new ContiPerfRule();
+//    @Test
+//    @PerfTest(invocations = 10000, threads = 20)
+//    @Required(max = 1200, average = 250)
+//    public void runMethodTest() throws IOException, InterruptedException {
+//        String[] args = ("165.227.137.80 /home/diana/Desktop/lab.txt").split(" ");
+//        app.run(args);
+//    }
+//
+//    @Test
+//    @PerfTest(invocations = 1, threads = 1)
+//    @Required(max = 1200, average = 250)
+//    public void serverTestRun() throws IOException, InterruptedException {
+//
+//    }
 
 
 }
