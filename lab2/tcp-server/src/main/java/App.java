fe59b007512b4604292c90dcc811f11a34b8af4f
dianaartiom <diana.artiom24@gmail.com>
2017-11-15 17:22:46 +0200

    Decoupled responnsibilities of writing the request and reading the response.

--- lab2/tcp-server/src/main/java/App.java
+++ lab2/tcp-server/src/main/java/App.java
@@ -1,5 +1,104 @@
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("App running.");
+
+    public App() {
+
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        App app = new App();
+        app.run(args);
+    }
+
+    public void run(String[] args) throws IOException, InterruptedException {
+        Socket socket = new Socket(args[0], 80);
+        socket.setSoTimeout(7000);
+
+        BufferedWriter out = new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+
+        sendMessage2(out, new File(args[1]));
+        readResponse(in);
+
+        out.close();
+        in.close();
+    }
+
+//    public void slowLoris(String[] args) throws IOException {
+//        Socket socket = new Socket(args[0], 80);
+//        socket.setSoTimeout(7000);
+//
+//        BufferedWriter out = new BufferedWriter(
+//                new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
+//        BufferedReader in = new BufferedReader(
+//                new InputStreamReader(socket.getInputStream()));
+//
+//        sendMessage(out, new File(args[1]));
+//        readResponse(in);
+//
+//        out.close();
+//        in.close();
+//    }
+
+    private static void sendMessage2(BufferedWriter out, File request) throws IOException, InterruptedException {
+        System.out.println(" * Request");
+
+        String temp = "";
+        for (String line : getContents(request)) {
+            System.out.println(line);
+            temp.concat(line);
+//            out.write(line + "\r\n");
+        }
+
+        /*bidlocod*/
+        for (int i = 0; i < temp.length(); i++) {
+            out.write(temp.charAt(i));
+
+            out.flush();
+            Thread.sleep(10);
+        }
+
+        out.write("\r\n");
+        out.flush();
+    }
+
+    private static void sendMessage(BufferedWriter out, File request) throws IOException {
+        System.out.println(" * Request");
+
+        for (String line : getContents(request)) {
+            System.out.println(line);
+            out.write(line + "\r\n");
+        }
+
+        out.write("\r\n");
+        out.flush();
+    }
+
+    private static void readResponse(BufferedReader in) throws IOException {
+        System.out.println("\n * Response");
+
+        String line;
+        while ((line = in.readLine()) != null) {
+            System.out.println(line);
+            /* trebuie de pus o conditie ca sa se opreasca dupa ce o venit 0 */
+        }
+    }
+
+    private static List<String> getContents(File file) throws IOException {
+        List<String> contents = new ArrayList<String>();
+
+        BufferedReader input = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = input.readLine()) != null) {
+            contents.add(line);
+        }
+        input.close();
+        return contents;
     }
 }
--- lab2/tcp-server/src/test/java/AppTest.java
+++ lab2/tcp-server/src/test/java/AppTest.java
@@ -1,29 +1,24 @@
-import client.Client;
 import org.databene.contiperf.PerfTest;
 import org.databene.contiperf.Required;
 import org.databene.contiperf.junit.ContiPerfRule;
-import org.databene.contiperf.junit.ContiPerfSuiteRunner;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import server.Server;
 
 import java.io.IOException;
 
-public class ClientTest {
+public class AppTest {
 
-    Client client = new Client();
-    Server server = new Server();
+    App app = new App();
 
     @Rule
     public ContiPerfRule i = new ContiPerfRule();
     @Test
     @PerfTest(invocations = 10000, threads = 20)
     @Required(max = 1200, average = 250)
-    public void clientTestRun() throws IOException, InterruptedException {
-
-        client.run();
+    public void runMethodTest() throws IOException, InterruptedException {
+        String[] args = ("165.227.137.80 /home/diana/Desktop/lab.txt").split(" ");
+        app.run(args);
     }
 
     @Test
@@ -31,7 +26,6 @@ public class ClientTest {
     @Required(max = 1200, average = 250)
     public void serverTestRun() throws IOException, InterruptedException {
 
-        server.run();
     }
 
 
