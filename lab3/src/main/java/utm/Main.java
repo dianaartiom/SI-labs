c23223d2132a153128ab494702f16e09e2a42b90
Diana Artiom <diana.artiom24@gmail.com>
2018-01-09 01:40:02 +0300

    Cleared main.

--- lab3/src/main/java/utm/Main.java
+++ lab3/src/main/java/utm/Main.java
@@ -8,24 +8,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-
-        EncryptedString es = new EncryptedString("RD QTAJ KTW YMJ WTRFS JRUNWJ NX ZSIJSNFGQD LWJFYJW YMFS KTW RDXJQK. YMJ LWJFYJXY JRUNWJ JAJW YT MFAJ JCNXYJI. N UQJILJ RD JYJWSFQ XJWANYZIJ FSI N FR KTWJAJW GTZSI YT XJWAJ NY, NS QNKJ FSI NS IJFYM. YMJD MFAJ RJWJQD LNAJS ZX: WTFIX, HJSYWFQ MJFYNSL, HTSHWJYJ, YMJ HFQJSIFW, FSI KQZXMNSL YTNQJYX FSI XJBJWX.");
-//        EncryptedString es = new EncryptedString("e");
-//        EncryptedString es = new EncryptedString("dk");
+        EncryptedString es = new EncryptedString("AB ZNRL MAX WXTWEBGX YHK MABL ETUHKTMHKR BL MAX MPXGMBXMA HY WXVXFUXK B PHNEW EBDX MH PBLA RHN ZHHW ENVD PBMA BM TGW ATOX T GBVX EBYX");
         EnMatcher rm = new EnMatcher();
-//        AbstractMatcher lm = new LanguageMatcher();
         System.out.println(rm.match(es, new EnLanguage(), new EnShifter()));
-//        assertNotEquals(lm.match(es), null);
-//        assertTrue(lm.match(es) == true);
-
-    }
-
-    public void compute(String[] args) {
-
-    }
-
-    public void compareToEnglishDictionary() {
-
     }
 }
